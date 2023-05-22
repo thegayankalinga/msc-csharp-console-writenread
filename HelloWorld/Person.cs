39ed51a7cf79151274f5c612d4a687fad9900072
Gayan Kalinga Bulathsinhala <bg15407@gmail.com>
2023-05-22 22:35:09 +0530

    initial commit with complete console application

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace HelloWorld
+{
+	public class Person
+	{
+		private String? name;
+		private int? age;
+		private String? email;
+		private String? addressLine01;
+		private String? addressLine02;
+		private String? postCode;
+
+		//use prop => public object MyProperty {get; set;}
+		public String Name { get { return name ?? ""; } set { name = value; } }
+		public int Age { get { return age ?? 0; } set { age = value; } }
+		public String Email { get { return email ?? ""; } set { email = value; } }
+		public String AddressLine01 { get { return addressLine01 ?? ""; } set { addressLine01 = value; } }
+		public String AddressLine02 { get { return addressLine02 ?? ""; } set { addressLine02 = value; } }
+		public String PostCode { get { return postCode ?? "";  } set { postCode = value; } }
+
+		public static void sayHi(String _name)
+		{
+			Console.Out.WriteLine("HI ! " + _name);
+		}
+
+		public void printDetail()
+		{
+			Console.Out.WriteLine(name!.ToUpperInvariant());
+            Console.Out.WriteLine(age);
+            Console.Out.WriteLine(email);
+            Console.Out.WriteLine(addressLine01);
+            Console.Out.WriteLine(addressLine02);
+            Console.Out.WriteLine(postCode);
+        }
+	}
+}
+
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Out.WriteLine("Hello World from Gayan, This is my first C#");
+            //String? name = null;
+            //name = Console.ReadLine();
+            //Console.Out.WriteLine("Hello " + name);
+
+            Person.sayHi("Stranger");
+            
+
+            Person person = new Person();
+            Console.Out.Write("Name: ");
+            person.Name = Console.ReadLine() ?? "";
+
+            Console.Out.Write("Age: ");
+            person.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Out.Write("Email: ");
+            person.Email = Console.ReadLine() ?? "";
+
+            Console.Out.Write("Address Line 01: ");
+            person.AddressLine01 = Console.ReadLine() ?? "";
+
+            Console.Out.Write("Address Line 02: ");
+            person.AddressLine02 = Console.ReadLine() ?? "";
+
+            Console.Out.Write("Post Code: ");
+            person.PostCode = Console.ReadLine() ?? "";
+
+
+            person.printDetail();
+
+        }
+    }
+}
